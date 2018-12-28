c88473ade02d2bfbf2e478d0300f4d3bacddec2e
Sean Bucholtz <sean.bucholtz@gmail.com>
2018-12-27 19:16:58 -0800

    Completed Unique Email Addresses

--- src/main/java/com/seanbucholtz/leetcodechallenges/uniqueemailaddresses/Solution.java
+++ src/main/java/com/seanbucholtz/leetcodechallenges/uniqueemailaddresses/Solution.java
@@ -1,7 +1,26 @@
 package com.seanbucholtz.leetcodechallenges.uniqueemailaddresses;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * Unique Email Addresses
  */
 class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new LinkedHashSet<String>(emails.length);
+        for(String email : emails) {
+            String[] emailSegments = email.split("@");
+            String localName = emailSegments[0];
+            localName = localName.replaceAll("\\.", "");
+            int plusCharIndex = localName.indexOf('+');
+            if(plusCharIndex > -1) {
+                localName = localName.substring(0, plusCharIndex);
+            }
+            String domainName = emailSegments[1];
+            String uniqueEmail = localName + "@" + domainName;
+            uniqueEmails.add(uniqueEmail);
+        }
+        return uniqueEmails.size();
+    }
 }
